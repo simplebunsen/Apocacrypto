7ec2f1f5a28cf48b76f47c84cb595c6bea9a91d6
simplebunsen <really@dontdoxme.com>
2020-04-23 14:05:37 +0200

    Stage 3: Including more characters in encryption, implementation of basic action switching and decrypting

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -6,15 +6,56 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String action = sc.nextLine();
+
         String message = sc.nextLine();
-        String output = "";
         int key = sc.nextInt();
+
+        switch(action){
+            case "enc":
+                encrypt(message, key);
+                break;
+            case "dec":
+                decrypt(message, key);
+                break;
+        }
+
+
+
+    }
+
+    private static void decrypt(String message, int key) {
+        String output = "";
+
         for(char c : message.toCharArray()){
             //97 - 122 lowercase letters
-            if(c <= 122 && c >= 97){
+            //32 - 126 basic symbols
+            if(c <= 126 && c >= 32){
                 char next;
-                if(c + key > 122){
-                    next = (char) (c + key - 26);
+                if(c - key < 0){
+                    next = (char) (c - key + 95);
+                } else {
+                    next = (char) (c - key);
+                }
+                output = output.concat(String.valueOf(next));
+            }else{
+                output = output.concat(String.valueOf(c));
+            }
+        }
+        System.out.println(output);
+
+    }
+
+    private static void encrypt(String message, int key) {
+        String output = "";
+
+        for(char c : message.toCharArray()){
+            //97 - 122 lowercase letters
+            //32 - 126 basic symbols
+            if(c <= 126 && c >= 32){
+                char next;
+                if(c + key > 126){
+                    next = (char) (c + key - 95);
                 } else {
                     next = (char) (c + key);
                 }
@@ -24,6 +65,5 @@ public class Main {
             }
         }
         System.out.println(output);
-
     }
 }
