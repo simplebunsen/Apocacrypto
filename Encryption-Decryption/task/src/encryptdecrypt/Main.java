c3078dbc3309cf5c664846cbc96bebebd5b31444
simplebunsen <really@dontdoxme.com>
2020-04-23 13:53:23 +0200

    Stage 2: Implementation of rudimentary key shift encryption system.

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,15 +1,23 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        String message = "we found a treasure!";
+        String message = sc.nextLine();
         String output = "";
+        int key = sc.nextInt();
         for(char c : message.toCharArray()){
             //97 - 122 lowercase letters
             if(c <= 122 && c >= 97){
-                int diff = c - 97;
-                char next = (char) (122 - diff);
+                char next;
+                if(c + key > 122){
+                    next = (char) (c + key - 26);
+                } else {
+                    next = (char) (c + key);
+                }
                 output = output.concat(String.valueOf(next));
             }else{
                 output = output.concat(String.valueOf(c));
