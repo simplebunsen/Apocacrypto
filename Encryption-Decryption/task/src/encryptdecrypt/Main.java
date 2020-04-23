ad7ecfb8eb2bb7fe33eaf926e7f8d85fe4724910
simplebunsen <really@dontdoxme.com>
2020-04-23 15:06:19 +0200

    Stage 5: Implement file import export

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,6 +1,10 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,37 +12,75 @@ public class Main {
         String action = "enc";
         String message = "";
         int key = 0;
+        String outFile = "";
 
         for (int i = 0; i < args.length; i++) {
             String s = args[i];
             switch (s) {
                 case "-mode":
                     action = args[++i];
+                    //System.out.println("loaded mode: " + action);
                     break;
                 case "-key":
                     key = Integer.parseInt(args[++i]);
+                    //System.out.println("loaded key: " + key);
                     break;
                 case "-data":
                     message = args[++i];
+                    //System.out.println("loaded date from string");
+                    break;
+                case "-in":
+                    if(message.isEmpty()){
+                        String input = "";
+                        try {
+                            input = new String(Files.readAllBytes(Paths.get(".\\" + args[++i])));
+                        } catch (IOException e) {
+                            System.out.println("Error: Input cant be read properly");
+                            e.printStackTrace();
+                        }
+                        //System.out.println("we loaded message: " + input);
+                        message = input;
+                    }
+                    break;
+                case "-out":
+                    outFile = args[++i];
+                    //System.out.println("made out file name : " + outFile);
                     break;
             }
         }
 
-
+        String result;
         switch(action){
             case "enc":
-                encrypt(message, key);
+                result = encrypt(message, key);
                 break;
             case "dec":
-                decrypt(message, key);
+                result = decrypt(message, key);
                 break;
+            default:
+                throw new IllegalStateException("Error: Unexpected value: " + action);
         }
 
-
-
+        outputResult(result, outFile);
     }
 
-    private static void decrypt(String message, int key) {
+    private static void outputResult(String result, String outFile) {
+        if(outFile.isEmpty()){
+            //System.out.println("output file name empty, printing result here");
+            System.out.println(result);
+            return;
+        }
+        //System.out.println("we have an output file name, saving to: " + outFile);
+        File file = new File(".\\" + outFile);
+        try (PrintWriter printWriter = new PrintWriter(file)) {
+            printWriter.print(result);
+            //System.out.println("printed");
+        } catch (IOException e) {
+            System.out.printf("Error: An exception occurs %s", e.getMessage());
+        }
+    }
+
+    private static String decrypt(String message, int key) {
         String output = "";
 
         for(char c : message.toCharArray()){
@@ -56,11 +98,10 @@ public class Main {
                 output = output.concat(String.valueOf(c));
             }
         }
-        System.out.println(output);
-
+        return output;
     }
 
-    private static void encrypt(String message, int key) {
+    private static String encrypt(String message, int key) {
         String output = "";
 
         for(char c : message.toCharArray()){
@@ -78,6 +119,6 @@ public class Main {
                 output = output.concat(String.valueOf(c));
             }
         }
-        System.out.println(output);
+        return output;
     }
 }
